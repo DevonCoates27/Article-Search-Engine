73c9d4c7dbc92ef2a1e91c986d7903d858abf45a
DevonCoates27 <94330740+DevonCoates27@users.noreply.github.com>
2023-01-25 13:31:46 -0600

    Second portion of cmake files

---
+++ tests.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/AVLMap.cpp" "CMakeFiles/tests.dir/AVLMap.cpp.o" "gcc" "CMakeFiles/tests.dir/AVLMap.cpp.o.d"
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/DocumentParser.cpp" "CMakeFiles/tests.dir/DocumentParser.cpp.o" "gcc" "CMakeFiles/tests.dir/DocumentParser.cpp.o.d"
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/IndexHandler.cpp" "CMakeFiles/tests.dir/IndexHandler.cpp.o" "gcc" "CMakeFiles/tests.dir/IndexHandler.cpp.o.d"
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/Word.cpp" "CMakeFiles/tests.dir/Word.cpp.o" "gcc" "CMakeFiles/tests.dir/Word.cpp.o.d"
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/documentID.cpp" "CMakeFiles/tests.dir/documentID.cpp.o" "gcc" "CMakeFiles/tests.dir/documentID.cpp.o.d"
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/porter2_stemmer-master/porter2_stemmer.cpp" "CMakeFiles/tests.dir/porter2_stemmer-master/porter2_stemmer.cpp.o" "gcc" "CMakeFiles/tests.dir/porter2_stemmer-master/porter2_stemmer.cpp.o.d"
+  "/Users/devoncoates/Documents/GitHub/assignment-4-search-engine-ryan-devon/test.cpp" "CMakeFiles/tests.dir/test.cpp.o" "gcc" "CMakeFiles/tests.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/AVLMap.cpp.o"
+  "CMakeFiles/tests.dir/AVLMap.cpp.o.d"
+  "CMakeFiles/tests.dir/DocumentParser.cpp.o"
+  "CMakeFiles/tests.dir/DocumentParser.cpp.o.d"
+  "CMakeFiles/tests.dir/IndexHandler.cpp.o"
+  "CMakeFiles/tests.dir/IndexHandler.cpp.o.d"
+  "CMakeFiles/tests.dir/Word.cpp.o"
+  "CMakeFiles/tests.dir/Word.cpp.o.d"
+  "CMakeFiles/tests.dir/documentID.cpp.o"
+  "CMakeFiles/tests.dir/documentID.cpp.o.d"
+  "CMakeFiles/tests.dir/porter2_stemmer-master/porter2_stemmer.cpp.o"
+  "CMakeFiles/tests.dir/porter2_stemmer-master/porter2_stemmer.cpp.o.d"
+  "CMakeFiles/tests.dir/test.cpp.o"
+  "CMakeFiles/tests.dir/test.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
